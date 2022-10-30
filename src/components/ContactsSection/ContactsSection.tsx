74a27823018e26a0d8a96a84daf8e3b8613d357b
Dalaskor <dalas.prog@gmail.com>
2022-10-30 14:29:56 +0400

    Add Contacts Section

--- src/components/ContactsSection/ContactsSection.tsx
+++ src/components/ContactsSection/ContactsSection.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import style from './ContactsSection.module.scss';
 import {ContactsBlock} from "../UI/ContactsBlock";
+import {SectionTitle} from "../UI/SectionTitle";
 
 export const ContactsSection = () => {
     return (
         <section className={style.contacts}>
-            <div className={style.contacts__container}></div>
+            <div className={style.contacts__container}>
+                <div className={style.contacts__header}>
+                    <SectionTitle titleText={"contacts"}/>
+                    <div className={style.contacts__line}></div>
+                </div>
+                <div className={style.contacts__body}>
+                    <div className={style.contacts__left}>
+                        <p>
+                            I’m interested in freelance opportunities. However, if you have other request or question,
+                            don’t hesitate to contact me.
+                        </p>
+                    </div>
+                    <div className={style.contacts__right}>
+                        <ContactsBlock title={'Message me here'} discord={'Dalaskor#5217'} email={"dalas.prog@gmail.com"}
+                                       telegram={"@DALASKOR"}/>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }

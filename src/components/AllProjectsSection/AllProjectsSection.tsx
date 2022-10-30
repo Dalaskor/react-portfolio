a2dec9e7fa222a84a687256329f8a085009bcbad
Dalaskor <dalas.prog@gmail.com>
2022-10-30 17:37:02 +0400

    Add decorative for projects page

--- src/components/AllProjectsSection/AllProjectsSection.tsx
+++ src/components/AllProjectsSection/AllProjectsSection.tsx
@@ -5,12 +5,15 @@ import {useProjects} from "../../hooks/projects";
 import {Loader} from "../UI/Loader";
 import {ErrorMessage} from "../UI/ErrorMessage";
 import {CardProject} from "../CardProject/CardProject";
+import dotsDecor from '../../assets/img/decor/dots.svg';
 
 export const AllProjectsSection = () => {
     const {projects, error, loading} = useProjects();
 
     return (
         <section className={style.projects}>
+            <img src={dotsDecor} className={style.projects__decor1}/>
+            <div className={style.projects__decor2}></div>
             <div className={style.projects__container}>
                 <SectionTitle titleText={"complete-apps"}/>
                 <div className={style.projects__list}>
--- src/components/AllSmallProjects/AllSmallProjects.tsx
+++ src/components/AllSmallProjects/AllSmallProjects.tsx
@@ -5,12 +5,16 @@ import {useProjectsSmall} from "../../hooks/projects-small";
 import {Loader} from "../UI/Loader";
 import {ErrorMessage} from "../UI/ErrorMessage";
 import {CardProject} from "../CardProject/CardProject";
+import dotsDecor from '../../assets/img/decor/dots.svg';
 
 export const AllSmallProjects = () => {
     const {projects, loading, error} = useProjectsSmall();
 
     return (
         <section className={style.projects}>
+            <div className={style.projects__decor1}></div>
+            <img src={dotsDecor} className={style.projects__decor2}/>
+            <img src={dotsDecor} className={style.projects__decor3}/>
             <div className={style.projects__container}>
                 <SectionTitle titleText={"small-projects"}/>
                 <div className={style.projects__list}>

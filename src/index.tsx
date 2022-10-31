13231533199345019a01191d5152bf112858f7e8
Dalaskor <dalas.prog@gmail.com>
2022-10-31 13:13:34 +0400

    F*ck go back

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {ModalState} from "./context/ModalContext";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <HashRouter>
+    <BrowserRouter>
         <ModalState>
             <App/>
         </ModalState>
-    </HashRouter>
+    </BrowserRouter>
 );

8eb077b227c37d9400c294a6a745e397d811def0
Dalaskor <dalas.prog@gmail.com>
2022-10-31 12:08:29 +0400

    Add Header and Footer mobile version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Header.module.scss'
 import {Logo} from "../Logo/Logo";
 import {Navbar} from "../Navbar/Navbar";
+import {useScrollLock} from "../../hooks/useScrollLock";
 
 export const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+    const {lockScroll, unLockScroll} = useScrollLock();
+
+    const burgerClickHandle = () => {
+        const menuState = !openMenu;
+        setOpenMenu(menuState);
+
+        if(menuState) {
+            lockScroll();
+        } else {
+            unLockScroll();
+        }
+    }
+
+    const navLinkClickHandle = () => {
+        setOpenMenu(false);
+        unLockScroll();
+    }
+
     return (
         <header className={style.header}>
             <div className={style.header__container}>
                 <Logo/>
-                <Navbar/>
+                <Navbar isShowMenu={openMenu} clickHandle={navLinkClickHandle}/>
+                <div className={style.header__burger} onClick={burgerClickHandle}>
+                    {!openMenu &&
+                    <svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="24" height="1" fill="#D9D9D9"/>
+                        <rect y="7" width="24" height="1" fill="#D9D9D9"/>
+                        <rect y="14" width="24" height="1" fill="#D9D9D9"/>
+                    </svg>}
+                    {openMenu &&
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect y="16.9706" width="24" height="1" transform="rotate(-45 0 16.9706)" fill="#D9D9D9"/>
+                        <rect x="0.707108" width="24" height="1" transform="rotate(45 0.707108 0)" fill="#D9D9D9"/>
+                    </svg>}
+                </div>
             </div>
         </header>
     )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,27 +2,32 @@ import React from 'react';
 import style from './Navbar.module.scss';
 import {NavLink} from "react-router-dom";
 
-export const Navbar = () => {
+interface NavbarProps {
+    isShowMenu: boolean,
+    clickHandle: () => void,
+}
+
+export const Navbar = ({isShowMenu, clickHandle}: NavbarProps) => {
     return (
-        <nav className={style.navbar}>
+        <nav className={isShowMenu ?  style.navbar_active : style.navbar}>
             <ul className={style.navbar__list}>
                 <li className={style.navbar__item}>
-                    <NavLink end to="/react-portfolio/" className={({isActive}) => isActive ? style.active : undefined}>
+                    <NavLink end onClick={clickHandle} to="/react-portfolio/" className={({isActive}) => isActive ? style.active : undefined}>
                         <span>#</span>home
                     </NavLink>
                 </li>
                 <li className={style.navbar__item}>
-                    <NavLink to="/react-portfolio/works" className={({isActive}) => isActive ? style.active : undefined}>
+                    <NavLink onClick={clickHandle} to="/react-portfolio/works" className={({isActive}) => isActive ? style.active : undefined}>
                         <span>#</span>works
                     </NavLink>
                 </li>
                 <li className={style.navbar__item}>
-                    <NavLink to="/react-portfolio/about" className={({isActive}) => isActive ? style.active : undefined}>
+                    <NavLink onClick={clickHandle} to="/react-portfolio/about" className={({isActive}) => isActive ? style.active : undefined}>
                         <span>#</span>about-me
                     </NavLink>
                 </li>
                 <li className={style.navbar__item}>
-                    <NavLink to="/react-portfolio/contacts" className={({isActive}) => isActive ? style.active : undefined}>
+                    <NavLink onClick={clickHandle} to="/react-portfolio/contacts" className={({isActive}) => isActive ? style.active : undefined}>
                         <span>#</span>contacts
                     </NavLink>
                 </li>
